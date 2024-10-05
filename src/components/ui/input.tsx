6a6b9e104895ab57a6d250877875af239aa209bb
Braimah-Abiola <braimahabiola.official@gmail.com>
2024-10-05 23:00:29 +0000

    Fix typ errors

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,23 +1,26 @@
 import * as React from "react";
 import { cn } from "@/lib/utils";
 
-const Input = React.forwardRef<
-  HTMLInputElement,
-  React.InputHTMLAttributes<HTMLInputElement>
->(({ className, type, ...props }, ref) => {
-  return (
-    <input
-      autoComplete="off"
-      type={type}
-      className={cn(
-        "flex h-11 w-full rounded-md focus:border focus:border-[#0096FA] focus:bg-[#343434]/50 backdrop-blur-md bg-[#343434] px-3 py-2 text-sm md:text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-white/70 text-white focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-ring focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50",
-        className
-      )}
-      ref={ref}
-      {...props}
-    />
-  );
-});
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <input
+        autoComplete="off"
+        type={type}
+        className={cn(
+          "flex h-11 w-full rounded-md focus:border focus:border-[#0096FA] focus:bg-[#343434]/50 backdrop-blur-md bg-[#343434] px-3 py-2 text-sm md:text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-white/70 text-white focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-ring focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
 Input.displayName = "Input";
 
 export { Input };
