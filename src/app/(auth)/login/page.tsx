b83faa3d73ca8ffeb2f9f088ff92af5aec77c004
Braimah-Abiola <braimahabiola.official@gmail.com>
2024-10-05 23:12:54 +0000

    Add suspense to more routes

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,19 @@
 import { LoginForm } from "@/components/auth/login-form";
+import { Loader2 } from "lucide-react";
+import { Suspense } from "react";
 
 const LoginPage = () => {
-  return <LoginForm />;
+  return (
+    <Suspense
+      fallback={
+        <div>
+          <Loader2 size={40} className=" animate-spin text-white" />
+        </div>
+      }
+    >
+      <LoginForm />
+    </Suspense>
+  );
 };
 
 export default LoginPage;
--- src/app/(auth)/new-password/page.tsx
+++ src/app/(auth)/new-password/page.tsx
@@ -1,7 +1,19 @@
 import { NewPasswordForm } from "@/components/auth/new-password-form";
+import { Loader2 } from "lucide-react";
+import { Suspense } from "react";
 
 const NewPasswordPage = () => {
-  return <NewPasswordForm />;
+  return (
+    <Suspense
+      fallback={
+        <div>
+          <Loader2 size={40} className=" animate-spin text-white" />
+        </div>
+      }
+    >
+      <NewPasswordForm />
+    </Suspense>
+  );
 };
 
 export default NewPasswordPage;
