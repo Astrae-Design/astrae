0ea1a91921019ffc65189c84e80dcc60b0f081d9
Braimah-Abiola <braimahabiola.official@gmail.com>
2024-10-05 23:08:37 +0000

    Add suspend boundary

--- src/app/(auth)/welcome/page.tsx
+++ src/app/(auth)/welcome/page.tsx
@@ -1,7 +1,19 @@
+import { Suspense } from "react";
 import { WelcomeForm } from "@/components/auth/welcome-form";
+import { Loader2 } from "lucide-react";
 
 const WelcomePage = () => {
-  return <WelcomeForm />;
+  return (
+    <Suspense
+      fallback={
+        <div>
+          <Loader2 size={40} className=" animate-spin text-white" />
+        </div>
+      }
+    >
+      <WelcomeForm />
+    </Suspense>
+  );
 };
 
 export default WelcomePage;
