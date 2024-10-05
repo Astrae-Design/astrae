340bd9c0912d16b5741c1483a6614a40ee035bae
Braimah-Abiola <braimahabiola.official@gmail.com>
2024-10-05 23:16:39 +0000

    Fix build error

--- src/app/(auth)/new-verification/page.tsx
+++ src/app/(auth)/new-verification/page.tsx
@@ -1,7 +1,19 @@
 import { NewVerificationForm } from "@/components/auth/new-verification-form";
+import { Loader2 } from "lucide-react";
+import { Suspense } from "react";
 
 const NewVerificationPage = () => {
-  return <NewVerificationForm />;
+  return (
+    <Suspense
+      fallback={
+        <div>
+          <Loader2 size={40} className=" animate-spin text-white" />
+        </div>
+      }
+    >
+      <NewVerificationForm />
+    </Suspense>
+  );
 };
 
 export default NewVerificationPage;
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,8 +1,18 @@
 import { RegisterForm } from "@/components/auth/register-form";
+import { Loader2 } from "lucide-react";
+import { Suspense } from "react";
 
 const RegisterPage = () => {
   return (
-    <RegisterForm />
+    <Suspense
+      fallback={
+        <div>
+          <Loader2 size={40} className=" animate-spin text-white" />
+        </div>
+      }
+    >
+      <RegisterForm />
+    </Suspense>
   );
 };
 
