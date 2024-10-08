c3ff63268b2ee67760902d70bbd2e0dd42172535
Braimah-Abiola <braimahabiola.official@gmail.com>
2024-10-08 10:03:40 +0000

    Fix type error

--- src/app/(landing)/(home)/page.tsx
+++ src/app/(landing)/(home)/page.tsx
@@ -2,7 +2,6 @@ import ChatWidget from "@/components/common/chat-widget";
 import GalleryShowcase from "@/components/custom/gallery";
 import CallToAction from "@/sections/cta";
 import FaqCentral from "@/sections/faq";
-import Features from "@/sections/features";
 import Hero from "@/sections/hero";
 import LogoTicker from "@/sections/logo-ticker";
 import Pricing from "@/sections/pricing";
