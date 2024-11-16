7a5f36d8de63e3b6f6b2fa6d8114567d3c33f35e
Braimah-Abiola <braimahabiola.official@gmail.com>
2024-11-16 14:28:52 +0000

    Fix

--- src/app/(landing)/(home)/page.tsx
+++ src/app/(landing)/(home)/page.tsx
@@ -5,7 +5,6 @@ import GalleryShowcase from "@/components/custom/gallery";
 import CallToAction from "@/sections/cta";
 import FaqCentral from "@/sections/faq";
 import Features from "@/sections/features";
-import Hero from "@/sections/hero";
 import LogoTicker from "@/sections/logo-ticker";
 import Pricing from "@/sections/pricing";
 import Reviews from "@/sections/reviews";
@@ -14,7 +13,7 @@ const Home = () => {
   return (
     <div className="w-full">
       <ChatWidget />
-      <Hero />
+      {/* <Hero /> */}
       <LogoTicker />
       <GalleryShowcase />
       <Features />
